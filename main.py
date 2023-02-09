0ce63c4aac49f47816839466303804f51c2aaacf
eshavyas <vyase58@gmail.com>
2023-02-08 22:59:32 -0800

    done

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    c= int (23 (73.40)) 
+    F=9/5*c+32
+    print("Fahrenheit is: {0 : .2F}" .format (F)) 
+   
     pass
 
 
 if __name__ == '__main__':
     main()
+   
+    pass
+
+
+
