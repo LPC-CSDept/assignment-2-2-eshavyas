09cbcbee5610fabbd18c11029a51ce458faeee6b
Kyuwoong Lee <kwooler@gmail.com>
2023-02-26 17:54:20 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    c= int (23 (73.40)) c  
+#     c= int (23 (73.40)) c  # your code
+    c= int (input())
     F= 9 /5 * c + 32  
     print ("Fahrenheit is {0 : .2F}") . __format__
     
